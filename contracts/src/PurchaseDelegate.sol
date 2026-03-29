68e408bd0d90339dd5a74de56f4068e6649762ed
Kurimochi <kurimochi.sec@gmail.com>
2026-03-29 16:39:35 +0900

    feat: Delegate ERC-20 payments

--- contracts/src/PurchaseDelegate.sol
+++ contracts/src/PurchaseDelegate.sol
@@ -2,14 +2,24 @@
 pragma solidity ^0.8.0;
 
 import {YakumoStore} from "./Yakumo.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract PurchaseDelegate {
     // --- EIP-7702 Delegate ---
 
-    function executePurchase(YakumoStore store, uint256 id, uint256 amount) external {
+    function executePurchaseWithEth(YakumoStore store, uint256 id, uint256 amount) external {
         (,,, uint256 price,) = store.works(id);
         uint256 total = price * amount;
 
         store.purchaseWithEth{value: total}(id, amount);
+    }
+
+    function executePurchaseWithErc20(YakumoStore store, uint256 id, uint256 amount) external {
+        (,,, uint256 price, address tokenContract) = store.works(id);
+        uint256 total = price * amount;
+
+        IERC20(tokenContract).approve(address(store), total);
+
+        store.purchaseWithErc20(id, amount);
     }
 }
--- contracts/test/PurchaseDelegate.t.sol
+++ contracts/test/PurchaseDelegate.t.sol
@@ -4,33 +4,31 @@ pragma solidity ^0.8.0;
 import {Test} from "forge-std/Test.sol";
 import {YakumoStore} from "../src/Yakumo.sol";
 import {PurchaseDelegate} from "../src/PurchaseDelegate.sol";
+import {TestERC20Token} from "./Tokens.sol";
 
 contract PurchaseDelegateTest is Test {
     YakumoStore store;
     PurchaseDelegate delegate;
 
-    uint256 price = 1 ether;
     address delegator;
     uint256 secretKey;
     address sponsor;
-    uint256 id;
 
     function setUp() public {
         store = new YakumoStore();
         delegate = new PurchaseDelegate();
 
         sponsor = makeAddr("1");
-        string memory metadataUri = "hogehoge";
-        bool transferable = false;
-        address tokenContract = address(0);
-
-        vm.prank(sponsor);
-        id = store.registerWork(metadataUri, transferable, price, tokenContract);
-
         (delegator, secretKey) = makeAddrAndKey("2");
     }
 
-    function testPurchaseDelegate() public {
+    function testExecutePurchaseWithEth() public {
+        string memory metadataUri = "hogehoge";
+        bool transferable = false;
+        uint256 price = 1 ether;
+        address tokenContract = address(0);
+        uint256 id = store.registerWork(metadataUri, transferable, price, tokenContract);
+
         vm.signAndAttachDelegation(address(delegate), secretKey);
 
         vm.txGasPrice(1);
@@ -41,18 +39,51 @@ contract PurchaseDelegateTest is Test {
         uint256 delegatorBalanceBefore = delegator.balance;
 
         vm.prank(sponsor);
-        PurchaseDelegate(payable(delegator)).executePurchase(store, id, 1);
+        PurchaseDelegate(payable(delegator)).executePurchaseWithEth(store, id, 1);
 
         assertEq(delegator.balance, delegatorBalanceBefore - price);
         assertLe(sponsor.balance, sponsorBalanceBefore);
         assertLt(sponsorBalanceBefore - sponsor.balance, price);
     }
 
-    function testPurchaseDelegateHaveNoEth() public {
+    function testExecutePurchaseWithEthHaveNoEth() public {
+        string memory metadataUri = "hogehoge";
+        bool transferable = false;
+        uint256 price = 1 ether;
+        address tokenContract = address(0);
+
+        vm.prank(sponsor);
+        uint256 id = store.registerWork(metadataUri, transferable, price, tokenContract);
+
         vm.signAndAttachDelegation(address(delegate), secretKey);
 
         vm.prank(sponsor);
         vm.expectRevert();
-        PurchaseDelegate(payable(delegator)).executePurchase(store, id, 1);
+        PurchaseDelegate(payable(delegator)).executePurchaseWithEth(store, id, 1);
+    }
+
+    function testExecutePurchaseWithErc20() public {
+        string memory metadataUri = "hogehoge";
+        bool transferable = false;
+        uint256 price = 1 ether;
+
+        TestERC20Token token = new TestERC20Token();
+        token.mint(delegator, price);
+
+        address tokenContract = address(token);
+        vm.prank(sponsor);
+        uint256 id = store.registerWork(metadataUri, transferable, price, tokenContract);
+
+        vm.signAndAttachDelegation(address(delegate), secretKey);
+
+        vm.txGasPrice(1);
+        vm.deal(sponsor, 10 ether);
+
+        vm.prank(sponsor);
+        PurchaseDelegate(delegator).executePurchaseWithErc20(store, id, 1);
+
+        assertEq(token.balanceOf(delegator), 0);
+        assertEq(token.balanceOf(sponsor), price);
+        assertLe(sponsor.balance, 10 ether);
     }
 }
